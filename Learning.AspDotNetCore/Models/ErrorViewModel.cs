6ddeceda0e6ffedc9ce6bdcdb3ab0bd0f5f3bb99
Tushar Sakhare <tushar.sakhare@imaginea.com>
2020-01-28 20:27:08 +0530

    first commit

---
+++ Learning.AspDotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Learning.AspDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Learning.DotnetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning.DotnetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
